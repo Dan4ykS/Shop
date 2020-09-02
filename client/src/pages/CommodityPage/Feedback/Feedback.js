2186bf09bdf211356149eda4a80506b8127d0187
Daniil <dan28012000@gmail.com>
2020-09-02 12:49:40 +0500

    bug fixes

--- client/src/pages/CommodityPage/Feedback/Feedback.js
+++ client/src/pages/CommodityPage/Feedback/Feedback.js
@@ -33,13 +33,8 @@ const Feedback = ({
 
   useEffect(() => {
     findUserReview(userName, reviews);
-    return () => {
-      if (userReview) {
-        clearUserReview();
-      }
-    };
-    // eslint-disable-next-line
-  }, []);
+    return () => clearUserReview();
+  }, [userName, reviews, clearUserReview, findUserReview]);
 
   useEffect(() => {
     updateLocalReview(userReview?.review ?? '');
